b159317534adb6e0dc6bbd2d605e1206efecab3f
Viral <129858326+VIRalZah@users.noreply.github.com>
2023-06-06 19:25:43 +0300

    added icon and color hack patches

--- patches.cpp
+++ patches.cpp
@@ -6,5 +6,7 @@ void hyperdahs()
   PatchManager pm;
   pm.addPatch("libcocos2dcpp.so", 0x2CDB14, "A7"); //levelPage count
   pm.addPatch("libcocos2dcpp.so", 0x21E5F2, "0B"); //remove anti-cheat
+  pm.addPatch("libcocos2dcpp.so", 0x228D2D, "E0"); //icon hack
+	pm.addPatch("libcocos2dcpp.so", 0x227E9C, "FF"); //color hack
   pm.Modify();
 }
