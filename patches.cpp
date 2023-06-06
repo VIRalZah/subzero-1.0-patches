0a73cbcc900d651af1a047a086bfdd4ff5bcf170
Viral <129858326+VIRalZah@users.noreply.github.com>
2023-06-06 19:13:30 +0300

    Update patches.cpp

--- patches.cpp
+++ patches.cpp
@@ -4,7 +4,7 @@ __attribute__((constructor))
 void hyperdahs()
 {
   PatchManager pm;
-	pm.addPatch("libcocos2dcpp.so", 0x2CDB14, "A7"); //levelPage count
-	pm.addPatch("libcocos2dcpp.so", 0x21E5F2, "0B"); //remove anti-cheat
+  pm.addPatch("libcocos2dcpp.so", 0x2CDB14, "A7"); //levelPage count
+  pm.addPatch("libcocos2dcpp.so", 0x21E5F2, "0B"); //remove anti-cheat
   pm.Modify();
 }
